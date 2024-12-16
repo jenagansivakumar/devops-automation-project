4e90941b44d5c0348532a7a6b5bc27f0f5b6f2ac
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-16 17:59:01 +0100

    added target groups

--- terraform/nat_gateway.tf
+++ terraform/nat_gateway.tf
@@ -7,5 +7,5 @@ resource "aws_eip" "nat_gateway_eip" {
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
   subnet_id = aws_subnet.public_subnet.id
-  depends_on = [ aws_internet_gateway.igw]
+  depends_on = [ aws_internet_gateway.igw ]
 }
--- terraform/target_groups.tf
+++ terraform/target_groups.tf
@@ -1,14 +1,29 @@
-resource "aws_lb_target_group" "vote_result_tg" {
-  name     = var.tg.name
-  port     = var.tg.port
-  protocol = var.tg.protocol
+resource "aws_lb_target_group" "tg1" {
+  name     = "berliners1"
+  port     = var.tg.berliners1.port
+  protocol = var.tg.berliners1.protocol
 
   health_check {
-    path                = var.tg.path
-    interval            = var.tg.interval
-    timeout             = var.tg.timeout
-    healthy_threshold   = var.tg.healthy_threshold
-    unhealthy_threshold = var.tg.unhealthy_threshold
+    path                = var.tg.berliners1.path
+    interval            = var.tg.berliners1.interval
+    timeout             = var.tg.berliners1.timeout
+    healthy_threshold   = var.tg.berliners1.healthy_threshold
+    unhealthy_threshold = var.tg.berliners1.unhealthy_threshold
+  }
+
+  vpc_id = aws_vpc.voting_app_vpc.id
+}
+resource "aws_lb_target_group" "tg2" {
+  name     = "berliners2"
+  port     = var.tg.berliners2.port
+  protocol = var.tg.berliners2.protocol
+
+  health_check {
+    path                = var.tg.berliners2.path
+    interval            = var.tg.berliners2.interval
+    timeout             = var.tg.berliners2.timeout
+    healthy_threshold   = var.tg.berliners2.healthy_threshold
+    unhealthy_threshold = var.tg.berliners2.unhealthy_threshold
   }
 
   vpc_id = aws_vpc.voting_app_vpc.id
