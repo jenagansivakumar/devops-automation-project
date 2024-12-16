b6cb0ee8324b86abd7e53e8b276a376f3740bb41
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-12-16 18:02:59 +0100

    added master instance

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -9,6 +9,17 @@ resource "aws_instance" "vote_instance" {
     Name = var.instances.vote.name
   }
 }
+resource "aws_instance" "master_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name = "master_instance"
+  }
+}
 
 resource "aws_instance" "redis_worker_instance" {
   ami           = var.ami_id
