9b2b96d8d6aa5411abbaf1b40adc4a3968d2a152
wessel-test <guiwessel@gmail.com>
2024-12-17 09:29:06 +0100

    adding alb

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -12,3 +12,30 @@ resource "aws_lb" "app_lb" {
   enable_cross_zone_load_balancing = var.alb_cross_zone
   idle_timeout                     = var.alb_idle_timeout
 }
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "forward"
+    forward {
+      target_group {
+        arn = aws_lb_target_group.tg1.arn
+      }
+    }
+  }
+}
+resource "aws_lb_listener" "tcp_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "forward"
+    forward {
+      target_group {
+        arn = aws_lb_target_group.tg2.arn
+      }
+    }
+  }
+}
--- terraform/target_groups.tf
+++ terraform/target_groups.tf
@@ -7,23 +7,23 @@ resource "aws_lb_target_group" "tg1" {
     path                = "/health-check-path"
     interval            = 30
     timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 
   vpc_id = aws_vpc.voting_app_vpc.id
 }
 resource "aws_lb_target_group" "tg2" {
   name     = "berliners2"
-  port     = 80
-  protocol = "HTTP"
+  port     = 6379
+  protocol = "TCP"
 
   health_check {
     path                = "/health-check-path"
     interval            = 30
     timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 
   vpc_id = aws_vpc.voting_app_vpc.id
